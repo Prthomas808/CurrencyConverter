814a9d23325b2a0abcf6ce49c6fc62b2bd86e9ba
prthomas808 <prthomas808@gmail.com>
2024-06-27 10:58:57 -0400

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Pedro Thomas on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
